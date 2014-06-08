55e13f90fe49f8074a2eeae2f997148c561f02b9
Mitchel Fields <msf12@hampshire.edu>
2014-06-08 18:40:15 -0400

    no changes made

--- Projectile.java
+++ Projectile.java
@@ -9,17 +9,16 @@ public class Projectile
 	
 		PhysicsObject pro = new PhysicsObject();
 		
-		double[] vel = new double[]{-1,0,0};
+		double[] vel = new double[]{10,10,10};
 		double[] acc = new double[]{-GRAVITY,0,0};
 		pro.setVel(vel);
-		pro.setPos(new double[]{10,0,0});
 		
-		System.out.println("Vel "+ pro.getVel()[0] +" Pos "+ pro.getPos()[0]);
+		System.out.println("Vel "+ vel[0] +" Pos "+ pro.getPos()[0]);
 		
-		while(pro.getPos()[0] > 0)
+		while(pro.getPos()[0] > -0.000001)
 		{
-			pro.timeStep(0.1);
-			System.out.println("Vel "+ pro.getVel()[0] +" Pos "+ pro.getPos()[0]);
+			pro.timeStepAcc(0.0001, acc);
+			System.out.println("Vel "+ vel[0] +" Pos "+ pro.getPos()[0]);
 		}
 		
 	}
