409c9baebe7294a6ccf9038c4f2e0be0f01c83b4
Berylbell <beryl7897@yahoo.com>
2014-06-08 22:54:14 -0400

    Update PhysicsObject.java
    Added spherical to cartesian transformation

--- PhysicsObject.java
+++ PhysicsObject.java
@@ -1,21 +1,20 @@
-
 public class PhysicsObject 
 {
 	private static int DIM =3;
-	
+
 	private double[] velocity = new double[DIM];
-	
+
 	private double[] position = new double[DIM];
-	
+
 	private double mass;
-	
+
 	//Create small, Massless, motionless
-	
+
 	public PhysicsObject ()
 	{
 		this(0);
 	}
-	
+
 	//Mass but no velocity
 	public PhysicsObject (double m)
 	{
@@ -24,38 +23,55 @@ public class PhysicsObject
 			velocity[i]=0;
 			position[i]=0;
 		}
-		
+
 		mass= m;
 	}
-	
+
 	public PhysicsObject (double[] vel, double m)
 	{
 		for(int i=0; i< DIM;i++)
 		{
 			position[i]=0;
 		}
-		
+
 		mass= m;
 		velocity = vel;
 	}
-	
+
 	public PhysicsObject (double[] vel, double[] pos, double m)
 	{
 		mass= m;
 		velocity = vel;
 		position = pos;
 	}
+
+	public PhysicsObject (double[] pos, double magnitude, double theta, double phi, double m)
+	{
+		mass = m;
+		position = pos ;
+		velocity = this.sphToCartCoodinates(magnitude, theta, phi);
+	}
 	
+	public double[] sphToCartCoodinates (double mag , double theta, double phi)
+	{
+		double[] vel = new double [DIM];
+		
+		vel[0] = mag * Math.sin(phi)*Math.cos(theta);
+		vel[1] = mag * Math.sin(phi)*Math.sin(theta);
+		vel[3] = mag * Math.cos(phi);
+		
+		return vel;
+	}
 	public double[] timeStep (double time)
 	{
-		
+
 		for(int i=0; i < DIM; i++)
 		{
 			position[i]= position[i] + velocity[i]*time;
 		}
 		return position;
 	}
-	
+
 	public double[] timeStepAcc (double time, double[] acc)
 	{
 		for(int i =0; i <DIM; i++)
@@ -68,22 +84,22 @@ public class PhysicsObject
 		}
 		return position;
 	}
-	
+
 	public double[] getVel ()
 	{
 		return velocity;
 	}
-	
+
 	public double[] getPos()
 	{
 		return position;
 	}
-	
+
 	public double getMass()
 	{
 		return mass;
 	}
-	
+
 	public void setVel (double[] newVel)
 	{
 		velocity = newVel;
@@ -93,7 +109,7 @@ public class PhysicsObject
 	{
 		position= newPos;
 	}
-	
+
 	public void setMass(double newMass)
 	{
 		mass= newMass;
